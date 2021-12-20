1b24e0c74571af9f7925054bd1ef355f98fe8fbc
eric <eric.schmidt1350@gmail.com>
2021-12-20 17:44:10 -0500

    Grid with cell hover effect

---
+++ script.js
@@ -0,0 +1,22 @@
+function genGrid(size) {
+    let container = document.querySelector('.container');
+
+    for(let i = 0;i < size;i++) {
+        let col = document.createElement('div');
+        col.classList.add('col');
+
+        for(let x = 0;x < size;x++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-square');
+
+            cell.addEventListener('mouseover', (e) => {
+                e.target.classList.add('hover-color');
+            });
+
+            col.appendChild(cell);
+        }
+        container.appendChild(col);
+    }
+}
+
+genGrid(16);
